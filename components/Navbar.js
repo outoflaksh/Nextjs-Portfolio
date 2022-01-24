1509854bab9474e8e9bec3f18873dd5415574c35
laksh <lakshyam38@gmail.com>
2022-01-24 17:43:01 +0530

    minor setups

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Nav.module.css'
+const Navbar = () => {
+    return (
+        <div className={styles.nav}>
+            <li className={styles.nav_link}>Lm</li>
+        </div>
+    )
+}
+
+export default Navbar
