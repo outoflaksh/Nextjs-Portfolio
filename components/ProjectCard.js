4146fee46a32191f4209f64af3ad50f2617c7e7c
laksh <lakshyam38@gmail.com>
2022-02-06 18:32:54 +0530

    added project section data

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,15 +1,12 @@
 import { FaGithub } from "react-icons/fa";
 import styles from "../styles/Project.module.css";
 
-const ProjectCard = () => {
+const ProjectCard = ({ details }) => {
   return (
     <div className={styles.card}>
-      <h1 className={styles.title}>Spotanew</h1>
+      <h1 className={styles.title}>{details.title}</h1>
       <FaGithub size="1.5rem" color="#333" />
-      <p className={styles.description}>
-        Discover exciting new music bla bla daljdlakd adjklaj fjaljflak
-        foiajfioa feiojfo faojfei afoajfoajfa kefj ejflaj.
-      </p>
+      <p className={styles.description}>{details.description}</p>
     </div>
   );
 };
--- components/Projects.js
+++ components/Projects.js
@@ -5,6 +5,24 @@ import ProjectCard from "./ProjectCard";
 
 import { FaRocket } from "react-icons/fa";
 
+const projectData = [
+  {
+    title: "Spotanew",
+    description:
+      "Discover new music by listening to the best parts of a randomly selected song among more than 1,000 songs spanning a dozen different genres.",
+  },
+  {
+    title: "Feather: Blog App",
+    description:
+      "Built a Markdown blog web app complete with a user model, custom profile pages, and dynamic routes.",
+  },
+  {
+    title: "Tenflix",
+    description:
+      "Get top 10 most similar content recommendations based on plot details of a user-selected Netflix title; calculated on a dataset of over 7000 different titles.",
+  },
+];
+
 const Projects = () => {
   return (
     <div className={styles.container}>
@@ -14,10 +32,9 @@ const Projects = () => {
       <section className={styles.main}>
         <h1 className={styles.heading}>What I've built so far:</h1>
         <section className={projectStyles.cards}>
-          <ProjectCard />
-
-          <ProjectCard />
-          <ProjectCard />
+          {projectData.map((project, idx) => (
+            <ProjectCard details={project} key={idx} />
+          ))}
         </section>
         <h1 className={styles.heading}>...and many more to come</h1>
       </section>
